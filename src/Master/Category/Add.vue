208b807a6e6c810b9794fd44889a1bb4f39e9bdb
hydra <hendryrpl@gmail.com>
2019-09-14 11:39:33 +0700

    menambahkan fungsi tambah data kategori

--- src/Master/Category/Add.vue
+++ src/Master/Category/Add.vue
@@ -5,14 +5,14 @@
             <div class="col-md-12">
                 <div class="main-card mb-3 card">
                     <div class="card-body">
-                        <form class="">
+                        <form @submit.prevent='AddCategory'>
                             <div class="position-relative form-group"><label for="name"
                                     class="">Name</label><input name="name" id="name"
                                     placeholder="Category Name" type="text" class="form-control" autocomplete="off">
                             </div>
                             <div class="position-relative form-group"><label for="description" class="">Description</label><textarea name="description" id="description" class="form-control"></textarea>
                             </div>
-                            <button class="mt-1 btn btn-primary">Submit</button>
+                            <button class="mt-1 btn btn-primary" type="submit">Submit</button>
                         </form>
                     </div>
                 </div>
@@ -34,11 +34,31 @@
             subheading: 'Halaman untuk menambah data kategori',
             icon: 'pe-7s-display1 icon-gradient bg-premium-dark',
             addlink: '',
+            category: {
+                name: '',
+                description: ''
+            }
             // editor: ClassicEditor,
             // editorData: '<p>Content of the editor.</p>',
             // editorConfig: {
             //     // The configuration of the editor.
             // }
         }),
+        methods: {  
+            AddCategory(){
+                axios.post('http://localhost:8000/api/category', this.$data.category)
+                    .then((response) => {
+                        this.$swal({
+                            title: 'Data kategori berhasil diinput',
+                            type: 'success',
+                            showCloseButton: false
+                        });
+                        this.$router.push('/master/category');
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            },
+        }
     }
 </script>
